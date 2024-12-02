7613a9ccaa77bde1fe5b2526f96d89298fc41c34
Dhruv <dhruvjain.dev@gmail.com>
2024-12-02 18:41:07 +0530

    Add 'new' button to recreate the grid

--- script.js
+++ script.js
@@ -21,15 +21,31 @@ function clickedClear() {
     let allHoveredDivs = document.querySelectorAll(".hovered");
 
     for (let i = 0; i < allHoveredDivs.length; i++) {
-        console.log(allHoveredDivs);    
         allHoveredDivs.item(i).classList.remove("hovered");
     }
 }
 
+function clickedNew() {
+    let n = parseInt(prompt("Enter number of squares per side:"));
+    if (n > 64) {
+        return alert("Squares exceed 64");
+    }
+
+    let childDivs = document.querySelectorAll(".row-div");
+
+    for (let i = 0; i < childDivs.length; i++) {
+        container.removeChild(childDivs.item(i));
+    }
+
+    generateDivs(n);
+}
+
 let container = document.querySelector(".container");
 
-generateDivs(30);
+generateDivs(16);
 
 let clearBtn = document.getElementById("clear");
+let newBtn = document.getElementById("new");
 
 clearBtn.addEventListener("click", clickedClear);
+newBtn.addEventListener("click", clickedNew);
